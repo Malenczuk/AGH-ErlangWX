8e5a66ba9229b7a62dd44bb9239a82ff77bbd69d
mdronski <dronskimichal@gmail.com>
2018-06-10 02:23:03 +0200

    resizable window works properly

--- src/gui.erl
+++ src/gui.erl
@@ -21,27 +21,37 @@ start() ->
 
 make_window() ->
   Server = wx:new(),
-  Frame = wxFrame:new(Server, -1, "Messenger", [{size,{400, 600}}]),
+  Frame = wxFrame:new(Server, -1, "Messenger", [{size,{600, 700}}]),
   Panel  = wxScrolledWindow:new(Frame),
   wxScrolledWindow:enableScrolling(Panel, false, true),
 
-  MainSizer =  wxBoxSizer:new(?wxVERTICAL),
-  MainSizer2 =  wxBoxSizer:new(?wxVERTICAL),
-  wxPanel:setSizer(Panel, MainSizer),
+  MainSizer = wxFlexGridSizer:new(1, 2, 10, 10),
+  ListSizer = wxFlexGridSizer:new(1, 1, 10, 10),
+  TextSizer = wxFlexGridSizer:new(2, 1, 10, 5),
+
+  Text1 = wxTextCtrl:new(Panel, 1001, [{style, ?wxTE_MULTILINE}, {style, ?wxTE_NO_VSCROLL}]),
+  Text2 = wxTextCtrl:new(Panel, 2001, [{style, ?wxTE_MULTILINE}, {style, ?wxTE_NO_VSCROLL}, {size, {100, 125}}]),
+  wxTextCtrl:setEditable(Text1, false),
+
+  CheckListBox = wxCheckListBox:new(Panel, 1234, [{size, {150, 100}}]),
+  wxSizer:add(ListSizer, CheckListBox, [{proportion, 1}, {flag, ?wxALL bor ?wxEXPAND}, {border, 5}]),
+  wxFlexGridSizer:addGrowableRow(ListSizer, 0, [{proportion, 1}]),
+  wxFlexGridSizer:addGrowableCol(ListSizer, 0, [{proportion, 1}]),
 
 
+  wxSizer:add(TextSizer, Text1, [{proportion, 1}, {flag, ?wxALL bor ?wxEXPAND}, {border, 5}]),
+  wxSizer:add(TextSizer, Text2, [{proportion, 1}, {flag, ?wxALL bor ?wxEXPAND}, {border, 5}]),
+
+  wxFlexGridSizer:addGrowableRow(TextSizer, 0, [{proportion, 1}]),
+  wxFlexGridSizer:addGrowableCol(TextSizer, 0, [{proportion, 1}]),
+
+  wxSizer:add(MainSizer, ListSizer, [{proportion, 1}, {flag, ?wxALL bor ?wxEXPAND}]),
+  wxSizer:add(MainSizer, TextSizer, [{proportion, 1}, {flag, ?wxALL bor ?wxEXPAND}]),
+  wxFlexGridSizer:addGrowableRow(MainSizer, 0, [{proportion, 1}]),
+  wxFlexGridSizer:addGrowableCol(MainSizer, 1, [{proportion, 1}]),
 
 
-
-  Text1 = wxTextCtrl:new(Panel, 1001, [{style, ?wxTE_MULTILINE}, {style, ?wxTE_NO_VSCROLL}, {size, {200, 75}}]),
-  Text2 = wxTextCtrl:new(Panel, 1001, [{style, ?wxTE_MULTILINE}, {style, ?wxTE_NO_VSCROLL}, {size, {200, 200}}]),
-  wxTextCtrl:setEditable(Text2, false),
-
-  wxSizer:add(MainSizer, Text2, [{flag, ?wxEXPAND}]),
-  wxSizer:addSpacer(MainSizer, 50),
-  wxSizer:add(MainSizer, Text1, [{flag, ?wxEXPAND}, {flag, ?wxALIGN_CENTER_VERTICAL}]),
-
-
+  wxPanel:setSizer(Panel, MainSizer, []),
   wxFrame:show(Frame),
 
   wxFrame:connect( Frame, close_window),
